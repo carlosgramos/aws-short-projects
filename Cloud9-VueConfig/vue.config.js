b2262b33c9c206d4d6575312452d545d23c26289
Carlos Ramos <carlos@carlosgramos.com>
2020-01-25 11:40:15 -0500

    Update vue.config.js
    Changing port to 8081

--- Cloud9-VueConfig/vue.config.js
+++ Cloud9-VueConfig/vue.config.js
@@ -1,11 +1,11 @@
  module.exports = {
     configureWebpack: {
         devServer: {
-            proxy: 'http://localhost:8080',
+            proxy: 'http://localhost:8081',
             disableHostCheck: true,
             host: "0.0.0.0",
-            port: 8080,
-            public: process.env.C9_HOSTNAME + ":8080",
+            port: 8081,
+            public: process.env.C9_HOSTNAME + ":8081",
         }
     }
 };
